cdbac8d16f063c2b0eece6d389b1664e5a0f50f4
AntonPetrenko <paa2017sh@gmail.com>
2023-02-19 23:26:02 +0200

    update2 src/js/02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,11 @@ import VimeoPlayer from '@vimeo/player';
 import { throttle } from 'lodash';
 
 
-const iframe = document.querySelector('#vimeo-player');
+// const iframe = document.querySelector('#vimeo-player');
 
-const player = new Vimeo.Player(iframe);
+// const player = new Vimeo.Player(iframe);
 
-player.on(
-    'timeupdate', throttle() => {
-    console.log("Scroll handler call every 300ms");
-}, 1000);
+// player.on(
+//     'timeupdate', throttle() => {
+//     console.log("Scroll handler call every 300ms");
+// }, 1000);
