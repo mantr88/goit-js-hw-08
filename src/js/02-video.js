f5ab7074d72120f8b076a091a43e420cccb9d11a
AntonPetrenko <paa2017sh@gmail.com>
2023-02-19 21:47:32 +0200

    added vmeo-player in script 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import VimeoPlayer from '@vimeo/player';
+import { throttle } from 'lodash';
 
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate', throttle () => {
+    console.log("Scroll handler call every 300ms");
+  }, 1000 )
