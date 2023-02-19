0e07aacd04ce09c598daac097f208c5e745b037d
AntonPetrenko <paa2017sh@gmail.com>
2023-02-19 13:36:30 +0200

    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+function makeGallery(items) {
+    return items.map(item => {
+        const listItem = document.createElement('li');
+        const itemLink = document.createElement('a');
+        itemLink.classList.add('gallery__item');
+        itemLink.href = `${item.original}`;
+
+        const itemImg = document.createElement('img');
+        itemImg.classList.add('gallery__image');
+        itemImg.src = `${item.preview}`;
+        itemImg.dataset.source = `${item.original}`;
+        itemImg.alt = `${item.description}`;
+
+        itemLink.append(itemImg);
+        listItem.append(itemLink);
+        return listItem
+
+    })
+}
+
+const galeryListRef = document.querySelector('.gallery');
+
+const items = makeGallery(galleryItems);
+galeryListRef.append(...items);
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt", });
