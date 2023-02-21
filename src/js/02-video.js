ce27bb613b779efd7a8006faef917194c5e44ad9
AntonPetrenko <paa2017sh@gmail.com>
2023-02-21 21:48:22 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import VimeoPlayer from '@vimeo/player';
-import { throttle } from 'lodash';
+const throttle = require('lodash.throttle');
 
 const CURRENT_TIME_KEY = "videoplayer-current-time"
 const iframe = document.querySelector('#vimeo-player');
@@ -8,12 +8,8 @@ const player = new Vimeo.Player(iframe);
 
 player.on(
     'timeupdate', throttle((data) => {
-        console.log("Scroll handler call every 1000ms");
         localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
         
     }, 1000));
 
-
 player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
-
-playbackVideo();
