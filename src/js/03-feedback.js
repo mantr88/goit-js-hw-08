864875f017e73c29ce72de3045df1eda23d9320b
AntonPetrenko <paa2017sh@gmail.com>
2023-02-21 22:32:54 +0200

    task-03 add start code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const throttle = require('lodash.throttle');
 
+const EMAIL_KEY = "email";
+const MESSAGE_KEY = "massage";
+const formRef = document.querySelector('.feedback-form');
+
+function inputsHandler(e) {
+    
+    e.preventDefault()
+    console.log('throtle work!')
+    localStorage.setItem(EMAIL_KEY, e.currentTarget.email.value);
+    localStorage.setItem(MESSAGE_KEY, e.currentTarget.message.value);
+
+
+
+}
+
+formRef.addEventListener('input', throttle(inputsHandler, 500));
